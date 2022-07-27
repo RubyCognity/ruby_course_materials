736ea926b9e63afbe93cca970b2158b2649fa156
Tomasz Kot <tomaszkot@macbook-pro-tomasz.home>
2022-07-27 09:39:26 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :posts
+  # get '/posts', to: 'posts#index'
+  # get '/posts/:id', to: 'posts#show'
+  # get '/posts/new', to: 'posts#new'
+  # get '/posts/:id/edit', to: 'posts#edit'
+  # post '/posts/:id', to: 'posts#create'
+  # put '/posts/:id', to: 'posts#update'
+  # put '/posts/:id', to: 'posts#update'
 end
